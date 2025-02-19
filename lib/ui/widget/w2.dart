063eeea15806b5727377ba17a61cc47ee38321e0
SantiagoR0214 <99304628+SantiagoR0214@users.noreply.github.com>
2025-02-19 10:35:41 -0500

    Actualizar w2.dart

--- lib/ui/widget/w2.dart
+++ lib/ui/widget/w2.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class W2 extends StatelessWidget {
-  const W2({super.key});
-  final double value = 0;
+  const W2({super.key, required this.value});
+  final double value ;
   @override
   Widget build(BuildContext context) {
+    double value2 = double.parse(value.toStringAsFixed(1)) - double.parse(value.toStringAsFixed(1)).toInt();
     return Container(
       key: const Key('W2'),
       width: 200,
       height: 200,
-      color: Colors.blueGrey, // should be grey when the decimal is bellow 0.4
+      color: double.parse(value2.toStringAsFixed(1)) > 0.4 ? Colors.blueGrey : const Color.fromARGB(255, 244, 54, 158), // should be grey when the decimal is bellow 0.4
     );
   }
 }
